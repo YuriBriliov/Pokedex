49f0951ea6725c08deae412f032d4d6e84a7ae1f
YBriliov <yuribriliov@gmail.com>
2023-03-27 11:43:54 -0700

    Filter realised

--- src/features/Filter/ui/Filter.jsx
+++ src/features/Filter/ui/Filter.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Filter = () => {
+const Filter = ({filter, fortByName, sortBySelect}) => {
+  const [sortName, setSortName] = useState('')
+
+  const getFIlterData = (event) =>{
+    return sortBySelect(event.target.value)
+  }
+
+  const setName = (event) =>{
+    setSortName(event.target.value)
+    return fortByName(event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1))
+  }
   return (
-    <div>
-      Filter
-    </div>
+    <form>
+      <select onChange={getFIlterData}>
+        <option value='Default'>Default</option>
+        {filter?.map((item)=>{
+          return(
+            <option value={item} key={item}>
+              {item}
+            </option>
+          )
+        })}
+      </select>
+      <input value={sortName} onChange={setName} placeholder='Sort by name...'/>
+    </form>
   );
 };
 
--- src/pages/Main/ui/Main.jsx
+++ src/pages/Main/ui/Main.jsx
@@ -1,13 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import ListElements from '../../../entities/ListElements/ui/ListElements';
+import Filter from '../../../features/Filter/ui/Filter';
 
 
 const Main = ({elements}) => {
 
+  const [ filteredData, setFilteredData ] = useState(elements)
+  const [filterOptions, setFilterElements] = useState([])
+
+
+  // Prepeated elements for filter options
+  useEffect(()=>{
+    setFilteredData(elements)
+    const filter = [...new Set(elements?.map((item)=>{
+      return item?.type
+    }).flat(Infinity))]
+    setFilterElements(filter)
+  },[elements])
+
+  // function filter by name
+  const nameSort = (arg) =>{
+    if(arg.length){
+      let searchWord = arg
+      const result = filteredData?.filter((item)=>{
+        if(item?.name?.english.includes(searchWord)){
+          return item?.name?.english.includes(arg)
+        }
+      })
+      setFilteredData(result)
+    }else{
+      setFilteredData(elements)
+    }
+  }
+
+  // function filter ty type
+  const typeSort = (arg) =>{
+    if(arg === 'Default'){
+      setFilteredData(elements)
+    } else {
+      const result = elements.filter((item)=>{
+        return item?.type.includes(arg)
+      })
+      setFilteredData(result)
+    }
+  }
+
   return (
     <div>
+        <Filter filter={filterOptions} fortByName={nameSort} sortBySelect={typeSort} />
 
-        {elements.length ? <ListElements elements={elements}/> : <div>...loading</div>}
+        {elements.length ? <ListElements elements={filteredData}/> : <div>...loading</div>}
      </div>
   );
 };
