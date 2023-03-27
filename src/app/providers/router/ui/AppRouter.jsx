09eac4b8b77f6a9c4daf897296ae1772fd5ce018
YBriliov <yuribriliov@gmail.com>
2023-03-27 10:25:26 -0700

    Created components

---
+++ src/app/providers/router/ui/AppRouter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppRouter = () => {
+  return (
+    <div>
+      Router
+    </div>
+  );
+};
+
+export default AppRouter;
---
+++ src/entities/ListElements/ui/ListElements.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListElements = () => {
+  return (
+    <div>
+      List Elements
+    </div>
+  );
+};
+
+export default ListElements;
