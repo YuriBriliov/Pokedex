377bfe14fd9bdb0db8b694f32dadd0689a6571a0
YBriliov <yuribriliov@gmail.com>
2023-03-27 10:49:53 -0700

    Get data from local file and add new attribute for all elements images where saved image name for creature

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react'
+// import { Header } from "../entities/Header";
 import './index.css'
+import data from '../data/pokedex.json'
+
 function App() {
+
+  const [baseData, setBaseData] = useState([])
+
+  const imagesContext = require.context('../data/images', false, /\.(png|jpe?g|svg)$/);
+  const imageFilenames = imagesContext.keys().map(key => key.split('/').pop());
+
+useEffect(()=>{
+  if(data.length){
+    const newData = data.map((item, index) => {
+     return {...item, image: imageFilenames[index]}
+    })
+    setBaseData(newData)
+  }
+},[])
+
+useEffect(()=>{
+  if(baseData){
+    console.log(baseData)
+  }
+},[baseData])
+
+
   return (
     <div className="App">
-      App
+      {/* <Header /> */}
+      Hello
     </div>
   );
 }
